91190a9b1b9a700bb3ae62d8066ab1d3fcc86783
Bruna Lima <brubsbsb270@gmail.com>
2023-03-17 10:39:53 -0300

    atualizando

---
+++ Módulo D/ex012.js
@@ -0,0 +1,6 @@
+var vel = 40
+console.log(`A velocidade do seu carro é ${vel} km/hora`)
+if(vel > 60){
+    console.log('voce ultrapassou a velocidade ')
+}
+console.log('dirija sempre usando o cinto')
---
+++ Módulo D/ex013.js
@@ -0,0 +1,7 @@
+var pais = 'Bélgica'
+console.log(`vivendo em ${pais}`)
+if (pais == 'Brasil'){
+    console.log('Voce é Brasileiro')
+} else {
+    console.log('Estrangeiro')
+}
